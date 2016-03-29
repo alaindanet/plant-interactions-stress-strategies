5084bc6333cb4c295518b5fa14509af9a337a70f
Alain D <alain.danet@caramail.fr>
2016-03-29 14:54:38 +0200

    data plant march entry

---
+++ code/data.R
@@ -0,0 +1,70 @@
+#'#'#########################################
+#'  Functions to manipulate the data sets  #'
+#'  Date: 22-03-2016
+#'  Author: Alain Danet
+#'##########################################'
+
+
+# Transform the january data set
+
+setwd("/home/alain/Documents/thesis/alicante/thesis_chap2_alicante/data/")
+
+
+load.plant <- function (data="data_january_plant.csv") {
+
+	data <- read.table(data, sep=";", dec=",", header=T)
+	data[, c("ind1","ind2","ind3")] <- matrix(c(rep(1,nrow(data)),
+			rep(2,nrow(data)),
+			rep(3,nrow(data))), ncol=3)
+
+	temp <- data[, c("ter", "plot", "label", "ms", "com", "watering")]
+	temp1 <- cbind(temp, data[, c("sp1", "ind1", "h1", "d1")])
+	colnames(temp1) <- c("ter", "plot", "label", "ms", "com", "watering",
+		"sp", "ind", "h", "d")
+	temp2 <- cbind(temp, data[, c("sp2", "ind2", "h2", "d2")])
+	colnames(temp2) <- c("ter", "plot", "label", "ms", "com", "watering",
+		"sp", "ind", "h", "d")
+	temp3 <- cbind(temp, data[, c("sp3", "ind3", "h3", "d3")])
+	colnames(temp3) <- c("ter", "plot", "label", "ms", "com", "watering",
+		"sp", "ind", "h", "d")
+
+	rbind(temp1,temp2,temp3)
+}
+
+# Transform the mars trait data set
+load.traits <- function (data="data_mars_traits.csv") {
+
+	data <- read.table(data, sep=";", dec=",", header=T)
+	data[, c("ind1","ind2","ind3")] <- matrix(c(rep(1,nrow(data)),
+			rep(2,nrow(data)),
+			rep(3,nrow(data))), ncol=3)
+	temp <- data[, c("ter", "plot", "label", "ms", "com", "watering")]
+
+	list.ind <- list()
+	for (i in 1:3) {
+		colonne <- c(paste("sp",i,sep=""),
+			paste("ind",i,sep=""),
+			paste("fresh_mass",i,sep=""),
+			paste("fresh_m_f",i,sep=""),
+			paste("dry_mass",i,sep=""),
+			paste("dry_m_f",i,sep="")
+			)
+		list.ind[[i]] <- cbind(temp, data[, colonne])
+		colnames(list.ind[[i]]) <- c("ter",
+			"plot",
+			"label",
+			"ms",
+			"com",
+			"watering",
+			"sp",
+			"ind",
+			"fresh_mass",
+			"fresh_m_f",
+			"dry_mass",
+			"dry_m_f")
+
+	}
+	rbind(list.ind[[1]], list.ind[[2]], list.ind[[3]])
+
+}
+
