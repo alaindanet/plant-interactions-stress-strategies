94576402befa98ef47212f84f475f380128fa35f
Alain D <alain.danet@caramail.fr>
2016-04-05 14:55:52 +0200

    march plant data added - new plots

--- code/data.R
+++ code/data.R
@@ -7,10 +7,9 @@
 
 # Transform the january data set
 
-setwd("/home/alain/Documents/thesis/alicante/thesis_chap2_alicante/data/")
 
 
-load.plant <- function (data="data_january_plant.csv") {
+LoadPlant <- function (data="../data/data_january_plant.csv") {
 
 	data <- read.table(data, sep=";", dec=",", header=T)
 	data[, c("ind1","ind2","ind3")] <- matrix(c(rep(1,nrow(data)),
@@ -28,11 +27,49 @@ load.plant <- function (data="data_january_plant.csv") {
 	colnames(temp3) <- c("ter", "plot", "label", "ms", "com", "watering",
 		"sp", "ind", "h", "d")
 
-	rbind(temp1,temp2,temp3)
+	temp <- rbind(temp1,temp2,temp3)
+
+	to.factor <- c("ter", "plot", "label","ms", "com", "watering", "sp", "ind")
+	for (i in 1:length(to.factor)) {
+		temp[,to.factor[i]] <- as.factor(temp[,to.factor[i]])
+	}
+
+	temp
+
 }
 
+LoadPlantMarch <- function (data="../data/data_mars_plant.csv") {
+
+	data <- read.table(data, sep=";", dec=",", header=T, na.strings=c("Dead", "C", " "))
+	data[, c("ind1","ind2","ind3")] <- matrix(c(rep(1,nrow(data)),
+			rep(2,nrow(data)),
+			rep(3,nrow(data))), ncol=3)
+
+	temp <- data[, c("ter", "plot", "label", "ms", "com", "watering")]
+	temp1 <- cbind(temp, data[, c("sp1", "ind1", "h1", "d1", "hm1")])
+	colnames(temp1) <- c("ter", "plot", "label", "ms", "com", "watering",
+		"sp", "ind", "h", "d", "hm")
+	temp2 <- cbind(temp, data[, c("sp2", "ind2", "h2", "d2", "hm2")])
+	colnames(temp2) <- c("ter", "plot", "label", "ms", "com", "watering",
+		"sp", "ind", "h", "d", "hm")
+	temp3 <- cbind(temp, data[, c("sp3", "ind3", "h3", "d3", "hm3")])
+	colnames(temp3) <- c("ter", "plot", "label", "ms", "com", "watering",
+		"sp", "ind", "h", "d", "hm")
+
+	temp.fin <- rbind(temp1,temp2,temp3)
+
+	to.factor <- c("ter", "plot", "label","ms", "com", "watering", "sp", "ind")
+	for (i in 1:length(to.factor)) {
+		temp.fin[,to.factor[i]] <- as.factor(temp.fin[,to.factor[i]])
+	}
+
+	return(temp.fin)
+
+}
+
+
 # Transform the mars trait data set
-load.traits <- function (data="data_mars_traits.csv") {
+load.traits <- function (data="../data/data_mars_traits.csv") {
 
 	data <- read.table(data, sep=";", dec=",", header=T)
 	data[, c("ind1","ind2","ind3")] <- matrix(c(rep(1,nrow(data)),
